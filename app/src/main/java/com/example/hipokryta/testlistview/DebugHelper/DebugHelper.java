4982574eab388763c425640376094f6257b76961
Artur G510 <ontur00@gmail.com>
2015-10-20 11:11:14 +0200

    Dodanie klasy do pomocy przy debugowaniu

---
+++ app/src/main/java/com/example/hipokryta/testlistview/DebugHelper/DebugHelper.java
@@ -0,0 +1,52 @@
+package com.example.hipokryta.testlistview.DebugHelper;
+
+import android.app.Activity;
+import android.nfc.Tag;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by radek on 2015-10-20.
+ * Klasa pomocniczna do wyswietlania informacji za pomoca
+ * Log-ów
+ * Toastów
+ * AlertDialog
+ */
+public class DebugHelper {
+
+    private static final String TAG = "APP_DEBUG";
+
+    /**
+     * Wyswietlanie wiadomosci za pomoca komunikatu Toast
+     * @param arg
+     * @param message
+     */
+    public void showToast(Activity arg, String message){
+
+        if(arg != null )
+            Toast.makeText(arg.getApplicationContext(), message, Toast.LENGTH_LONG);
+
+        Log.d(TAG, "context == null nie mozna wyswietlic wiadomosci");
+    }
+
+    /**
+     * Wyswietlanie wiadomosci za pomoca Log.d
+     * @param message
+     */
+    public void showLog(String message){
+        Log.d(TAG, message);
+    }
+
+    /**
+     * Wyswietlanie wiadomosci za pomoca AlertDialog
+     * @param message
+     */
+    public void showAlertDialog(Activity arg, String message){
+        AlertDialog.Builder alert = new AlertDialog.Builder(arg);
+        alert.setTitle("DEBUG");
+        alert.setMessage(message);
+        alert.setPositiveButton("OK", null);
+        alert.show();
+    }
+}
--- app/src/main/java/com/example/hipokryta/testlistview/MyOpenGL/MyOpenGLActivity.java
+++ app/src/main/java/com/example/hipokryta/testlistview/MyOpenGL/MyOpenGLActivity.java
@@ -1,16 +1,51 @@
 package com.example.hipokryta.testlistview.MyOpenGL;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.example.hipokryta.testlistview.R;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 public class MyOpenGLActivity extends Activity {
+
+    private GLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        disableTitle();
+
+        glSurfaceView = new GLSurfaceView(this);
+        glSurfaceView.setRenderer(new MyOpenGLRender());
         setContentView(R.layout.activity_my_open_gl);
     }
 
+    private void disableTitle() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+
+    private class MyOpenGLRender implements GLSurfaceView.Renderer {
+        @Override
+        public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+//            Log
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl10) {
+
+        }
+    }
 }
